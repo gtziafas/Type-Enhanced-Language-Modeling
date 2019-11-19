75c4578dc9526f4f7c3b7fc4bf9ae9119b79cce9
gtziafas <pullmypill@hotmail.com>
2019-11-19 12:14:26 +0200

    pretty looks

--- src/model/type_factored_lm.py
+++ src/model/type_factored_lm.py
@@ -5,18 +5,19 @@ from src.model.masked_encoder import MaskedEncoder, MaskedEncoderLayer
 class TypeFactoredLM(Module):
     def __init__(self, masked_encoder: Module, type_classifier: Module, word_classifier: Module) -> None:
         super(TypeFactoredLM, self).__init__()
-        self.MaskedEncoder = masked_encoder
-        self.TypeClassifier = type_classifier
-        self.WordClassifier = word_classifier
+        self.masked_encoder = masked_encoder
+        self.type_classifier = type_classifier
+        self.word_classifier = word_classifier
 
     def forward(self, word_embeds: FloatTensor, msk: LongTensor) -> Tuple[FloatTensor, FloatTensor]:
         batch_size, num_words, d_model = word_embeds.shape[0:3]
 
-        word_embeds = PositionalEncoding(b=batch_size, n=num_words, ad_model=d_model, device=word_embeds.device) + word_embeds 
-        encoded = self.MaskedEncoder((word_embeds, msk))[0]
-        word_predictions = self.WordClassifier(encoded)
-        type_predictions = self.TypeClassifier(encoded)
-        return word_predictions, type_predictions
+        word_embeds = PositionalEncoding(b=batch_size, n=num_words, d_model=d_model, device=word_embeds.device) + word_embeds 
+        encoded = self.masked_encoder((word_embeds, msk))[0]
+        word_preds = self.word_classifier(encoded)
+        type_preds = self.type_classifier(encoded)
+        
+        return word_preds, type_preds
 
 def test():
     d_model = 512 
