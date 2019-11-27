bebc6eea30ec5fd11420f2838667971037af11d9
konstantinosKokos <konstantinos@riseup.net>
2019-11-27 17:11:15 +0100

    minimal refaxtor

--- TypeLM/utils/lassy_extractor.py
+++ TypeLM/utils/lassy_extractor.py
@@ -1,5 +1,4 @@
 from TypeLM.utils.imports import *
-from TypeLM.utils.utils import mask_indices, mask_sampling
 import xml.etree.cElementTree as et
 
 from glob import glob
@@ -11,22 +10,10 @@ from tqdm import tqdm
 
 from itertools import chain
 
-Sample = Sequence[Tuple[str, str]]
-Samples = Sequence[Sample]
-
-
-directory = '/run/user/1000/gvfs/smb-share:server=solis-storage01,share=uil-ots$/LASSY 4.0/LassyLarge'
 
 _T1 = TypeVar('_T1')
 
-
-def mask_sample(sample: Sample, random_chance: float = 0.15, mask_token: str = '[MASK]') -> Tuple[Sample, ints]:
-    words, types = list(zip(*sample))
-    masked_indices = mask_indices(len(words), random_chance)
-    # todo. actual sampling
-    words = mask_sampling(words, masked_indices, lambda: mask_token)
-    types = mask_sampling(types, masked_indices, lambda: mask_token)
-    return list(zip(words, types)), masked_indices
+_directory = '/run/user/1000/gvfs/smb-share:server=solis-storage01,share=uil-ots$/LASSY 4.0/LassyLarge'
 
 
 def unzip(file: str) -> str:
@@ -59,7 +46,6 @@ def get_files(inner_dir: str) -> Sequence[str]:
 
 class DatasetMaker(object):
     def __init__(self, outer_dir: str):
-        # /run/user/1000/gvfs/smb-share:server=solis-storage01,share=uil-ots$/LASSY 4.0/LassyLarge'
         inner_dirs = list(map(lambda x: outer_dir + '/' + x, os.listdir(outer_dir)))
         inner_dirs = list(filter(os.path.isdir, inner_dirs))
         self.inner_dirs = list(map(lambda x: x + '/COMPACT/', inner_dirs))
@@ -76,15 +62,13 @@ class DatasetMaker(object):
 
     @staticmethod
     def file_to_projections(file: str, projector: Callable[[et.ElementTree], _T1]) -> Sequence[_T1]:
-        try:
-            return list(filter(lambda x: x is not None,
-                               list(map(lambda tree: projector(tree), DatasetMaker.file_to_trees(file)))))
-        except TimeoutError:
-            print('Timed out on file {}'.format(file))
-            return []
+        return list(filter(lambda x: x is not None,
+                           list(map(lambda tree: projector(tree), DatasetMaker.file_to_trees(file)))))
 
     def iterate_data(self, projector: Callable[[et.ElementTree], _T1], save_to: str = 'dump', start_from: int = 0):
         with open(save_to, 'a') as f:
+            if start_from == 0:
+                f.write('<?xml version="1.0"?>\n<sentences>\n')
             for file in tqdm(self.filelist[start_from:]):
                 projections = list(chain.from_iterable(self.file_to_projections(file, projector)))
                 xmls = '\n'.join(list(map(lambda p: self.projection_to_xml(*p), projections)))
@@ -92,6 +76,7 @@ class DatasetMaker(object):
 
     @staticmethod
     def projection_to_xml(name: str, words: Sequence[str], types: Sequence[str]) -> str:
+        words = list(map(DatasetMaker.sanitize_word, words))
         word_str = '\t<words>\n\t\t' + \
                    '\n\t\t'.join(list(map(lambda word: '<word>"' + word + '"</word>', words))) + \
                    '\n\t</words>'
@@ -101,11 +86,13 @@ class DatasetMaker(object):
 
         return '<sentence id="' + name + '">' + '\n' + word_str + '\n' + type_str + '\n</sentence>'
 
+    @staticmethod
+    def sanitize_word(word: str) -> str:
+        return word
+
 
 def do_single_file(file: str, projector):
     return DatasetMaker.file_to_projections(file, projector)
 
 # dsmk = DatasetMaker(directory)
-# from LassyExtraction.main2 import compose
-
 # dsmk.iterate_data(compose)
