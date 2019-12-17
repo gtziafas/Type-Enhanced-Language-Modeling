a7e9fcc8c9b75108c1cd55dca04acc659e3f3708
gtziafas <pullmypill@hotmail.com>
2019-12-17 16:26:11 +0200

    losses_stuff

--- TypeLM/model/loss.py
+++ TypeLM/model/loss.py
@@ -23,16 +23,22 @@ class MixedLoss(Module):
         type_loss = self.type_loss(type_predictions, type_truth)
         return language_loss + self.kappa * type_loss
 
+    def get_both_losses(self, word_predictions: Tensor, word_truth: LongTensor,
+                type_predictions: Tensor, type_truth: LongTensor, mask: LongTensor) -> Tensor:
+        language_loss = masked_loss_wrapper(self.language_loss)(word_predictions, word_truth, mask)
+        type_loss = self.type_loss(type_predictions, type_truth)
+        return language_loss, type_loss
 
-# def example():
-# 	generic_loss = torch.nn.CrossEntropyLoss
-# 	generic_kwargs = {'ignore_index': 0, 'reduction': 'mean'}
-# 	mixed_loss = MixedLoss(generic_loss, generic_loss, generic_kwargs, generic_kwargs, 0.1)
-# 	wp = torch.rand(32, 12, 5).view(-1, 5)
-# 	tp = torch.rand(32, 12, 27).view(-1, 27)
-# 	wt = torch.randint(size=(32, 12), low=0, high=5).view(-1)
-# 	tt = torch.randint(size=(32, 12), low=0, high=27).view(-1)
-# 	wm = torch.randint(size=(32, 12), low=0, high=2).view(-1)
-# 	print(mixed_loss(wp, wt, tp, tt, wm))
+
+def example():
+	generic_loss = torch.nn.CrossEntropyLoss
+	generic_kwargs = {'ignore_index': 0, 'reduction': 'mean'}
+	mixed_loss = MixedLoss(generic_loss, generic_loss, generic_kwargs, generic_kwargs, 0.1)
+	wp = torch.rand(32, 12, 5, device='cuda').view(-1, 5)
+	tp = torch.rand(32, 12, 27, device='cuda').view(-1, 27)
+	wt = torch.randint(size=(32, 12), low=0, high=5, device='cuda').view(-1)
+	tt = torch.randint(size=(32, 12), low=0, high=27, device='cuda').view(-1)
+	wm = torch.randint(size=(32, 12), low=0, high=2, device='cuda').view(-1)
+	print(mixed_loss(wp, wt, tp, tt, wm))
 
 
