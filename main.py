9ecdc774e6e378d691301d7c001ee5734ad023f8
gtziafas <pullmypill@hotmail.com>
2020-03-13 19:34:26 +0200

    opt pre-train step

--- main.py
+++ main.py
@@ -114,9 +114,9 @@ def main(load_id: Optional[str], save_id: Optional[str]):
     pre_train_epochs = 0 
     if load_id is not None:
         model, _opt, pre_train_epochs, _ = load_model(model_path=load_id, model=model, opt=_opt)
-    _opt.step = pre_train_epochs * steps_per_epoch
     opt = CustomLRScheduler(_opt, [linear_scheme], warmup_steps=1e05, goal_lr=1e-04, decrease_rate=1e-11, min_lr=1e-07)
-
+    opt._step = pre_train_epochs * steps_per_epoch
+    
     print('\nStarted training..') 
     sys.stdout.flush()
     for step in range(num_epochs * steps_per_epoch):
