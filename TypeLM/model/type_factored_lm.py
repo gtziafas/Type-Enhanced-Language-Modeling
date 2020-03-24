b29ef738cc3498c0f93d05c04245392263940326
konstantinosKokos <konstantinos@riseup.net>
2020-03-24 12:13:37 +0100

    smooth guidance in forward

--- TypeLM/model/type_factored_lm.py
+++ TypeLM/model/type_factored_lm.py
@@ -1,13 +1,13 @@
 from TypeLM.utils.imports import *
-from TypeLM.utils.utils import PositionalEncoder, count_parameters
+from TypeLM.utils.utils import PositionalEncoder, count_parameters, LabelSmoother
 from TypeLM.model.masked_encoder import EncoderLayer, LayerWeighter, Encoder
 
 
 class TypeFactoredLM(Module):
     def __init__(self, masked_encoder: Module, type_classifier: Module, type_embedder: Module,
                  fusion: Module, masked_encoder_kwargs: Dict, num_words: int, type_classifier_kwargs: Dict,
-                 type_embedder_kwargs: Dict, fusion_kwargs: Dict, padding_idx: int = 0,
-                 dropout_rate: float = 0.1) -> None:
+                 type_embedder_kwargs: Dict, fusion_kwargs: Dict, label_smoother_kwargs: Dict,
+                 padding_idx: int = 0, dropout_rate: float = 0.1) -> None:
         super(TypeFactoredLM, self).__init__()
         self.masked_encoder = masked_encoder(**masked_encoder_kwargs)
         self.layer_weighter = LayerWeighter(masked_encoder_kwargs['num_layers']-2)
@@ -18,13 +18,20 @@ class TypeFactoredLM(Module):
                                        padding_idx=padding_idx)
         self.positional_encoder = PositionalEncoder(dropout_rate)
         self.dropout = Dropout(dropout_rate)
+        self.label_smoother = LabelSmoother(**label_smoother_kwargs)
 
-    def forward(self, word_ids: LongTensor, pad_mask: LongTensor) -> Tuple[Tensor, Tensor]:
+    def forward(self, word_ids: LongTensor, pad_mask: LongTensor,
+                type_guidance: Optional[LongTensor] = None,
+                smoothing: Optional[float] = None) -> Tuple[Tensor, Tensor]:
         layer_outputs = self.get_all_vectors(word_ids, pad_mask)
         weighted = self.layer_weighter(layer_outputs[1:-2])
         type_preds = self.type_classifier(self.dropout(weighted))
-        type_preds_activated = type_preds.softmax(dim=-1)
-        type_embeddings = self.dropout(self.type_embedder(type_preds_activated))
+        if type_guidance is None:
+            type_preds_activated = type_preds.softmax(dim=-1)
+            type_embeddings = self.type_embedder(type_preds_activated)
+        else:
+            type_probs = self.label_smoother(type_guidance, smoothing)
+            type_embeddings = self.type_embedder(type_probs)
         word_preds = self.word_classifier(self.fusion(type_embeddings, layer_outputs[-1]))
         return word_preds, type_preds
 
@@ -33,9 +40,6 @@ class TypeFactoredLM(Module):
         weighted = self.layer_weighter(layer_outputs[1:-2])
         type_preds = self.type_classifier(weighted)
         return type_preds
-
-    def forward_lm(self, word_ids: LongTensor, pad_mask: LongTensor) -> Tensor:
-        return self.forward(word_ids, pad_mask)[0]
 
     def get_all_vectors(self, word_ids: LongTensor, pad_mask: LongTensor) -> Tensor:
         word_embeds = self.word_embedder(word_ids)
