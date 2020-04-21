2002042a88af99e8fc76cfb7ccae4c7d5de6d1ff
konstantinosKokos <konstantinos@riseup.net>
2020-04-21 15:49:11 +0200

    prints

--- TypeLM/finetuning/finetune_ner.py
+++ TypeLM/finetuning/finetune_ner.py
@@ -2,6 +2,7 @@ from nlp_nl.nl_eval.tasks import NER
 from nlp_nl.prepare_data import get_ner
 from TypeLM.finetuning.fineloaders import make_token_train_dl, make_token_test_dl
 from TypeLM.finetuning.finetuners import TokenClassification, AdamW, CrossEntropyLoss
+from TypeLM.utils.imports import *
 from tqdm import tqdm
 from main import default_model
 import sys
@@ -9,15 +10,18 @@ import sys
 NUM_EPOCHS = 500
 
 
-def main(data_folder: str, result_folder: str, model_path: str, device: str):
+def main(data_folder: str, result_folder: str, model_path: str, device: str, ):
     get_ner(data_folder, result_folder)
     ner = NER(data_folder)
     trainset = list(map(lambda x: list(zip(*x[0])), tqdm(list(zip(ner.task.class_train_data)))))
     valset = list(map(lambda x: list(zip(*x[0])), tqdm(list(zip(ner.task.class_dev_data)))))
     testset = list(map(lambda x: list(zip(*x[0]))[0], tqdm(list(zip(ner.task.class_test_data)))))
     train_dl = make_token_train_dl(trainset, device=device)
+    print('Done making train loader.')
     val_dl = make_token_train_dl(valset, device=device)
+    print('Done making val loader.')
     test_dl = make_token_test_dl(testset, device=device)
+    print('Done making test loader.')
     del trainset, valset, testset
 
     model = TokenClassification(default_model, model_path, len(ner.task.classes) + 1).to(device)
