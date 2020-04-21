8c014f0633402c362634fcbd8a0ea9f41432090f
gtziafas <pullmypill@hotmail.com>
2020-04-21 16:37:43 +0300

    flush std

--- TypeLM/finetuning/finetune_ner.py
+++ TypeLM/finetuning/finetune_ner.py
@@ -4,14 +4,14 @@ from TypeLM.finetuning.fineloaders import make_token_train_dl, make_token_test_d
 from TypeLM.finetuning.finetuners import TokenClassification, AdamW, CrossEntropyLoss
 from tqdm import tqdm
 from main import default_model
-
+import sys
 
 NUM_EPOCHS = 500
 
 
 def main(data_folder: str, result_folder: str, model_path: str, device: str):
     get_ner(data_folder, result_folder)
-    ner = NER(data_folder='./nlp_nl/data/NER/')
+    ner = NER(data_folder)
     trainset = list(map(lambda x: list(zip(*x[0])), tqdm(list(zip(ner.task.class_train_data)))))
     valset = list(map(lambda x: list(zip(*x[0])), tqdm(list(zip(ner.task.class_dev_data)))))
     testset = list(map(lambda x: list(zip(*x[0]))[0], tqdm(list(zip(ner.task.class_test_data)))))
@@ -30,3 +30,4 @@ def main(data_folder: str, result_folder: str, model_path: str, device: str):
         print(f'Train Loss: {train_loss}\t Train Acc: {train_acc}')
         val_loss, val_acc = model.eval_epoch(val_dl, loss_fn)
         print(f'Train Loss: {val_loss}\t Train Acc: {val_acc}')
+        sys.stdout.flush()
