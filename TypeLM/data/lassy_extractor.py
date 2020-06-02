4c7fd10468a8c4a5489b2c3f49d539a2c64fb7e3
konstantinosKokos <konstantinos@riseup.net>
2020-06-02 15:38:01 +0200

    refactor extractor

--- TypeLM/data/lassy_extractor.py
+++ TypeLM/data/lassy_extractor.py
@@ -1,19 +1,23 @@
-from TypeLM.utils.imports import *
-import xml.etree.cElementTree as et
+from typing import Sequence, Tuple, Optional, Callable
+import xml.etree.cElementTree as Tree
 
 from glob import glob
 import os
 from functools import reduce
 import subprocess
 
+from itertools import chain
 from tqdm import tqdm
 
-from itertools import chain
-
-
-_T1 = TypeVar('_T1')
+from LassyExtraction.utils.tools import get_types, get_words
+from LassyExtraction.extraction import extractor
+from LassyExtraction.transformations import transformer
 
 _directory = '/run/user/1000/gvfs/smb-share:server=solis-storage01,share=uil-ots$/LASSY 4.0/LassyLarge'
+
+
+Projection = Tuple[Sequence[str], Sequence[str]]
+Projections = Sequence[Projection]
 
 
 def unzip(file: str) -> str:
@@ -45,48 +49,65 @@ def get_files(inner_dir: str) -> Sequence[str]:
 
 
 class DatasetMaker(object):
-    def __init__(self, outer_dir: str):
-        inner_dirs = list(map(lambda x: outer_dir + '/' + x, os.listdir(outer_dir)))
-        inner_dirs = list(filter(os.path.isdir, inner_dirs))
-        self.inner_dirs = list(map(lambda x: x + '/COMPACT/', inner_dirs))
-        filelist = reduce(lambda x, y: x+y, map(get_files, self.inner_dirs))
-        self.filelist = sorted(filelist)
+    def __init__(self, outer_dir: Optional[str], filelist: Optional[Sequence[str]]):
+        if filelist is None and outer_dir is None:
+            raise ValueError('Provide either an outer dir or a filelist.')
+        elif filelist is None:
+            print('Crawling..')
+            inner_dirs = list(map(lambda x: outer_dir + '/' + x, os.listdir(outer_dir)))
+            inner_dirs = list(filter(os.path.isdir, inner_dirs))
+            self.inner_dirs = list(map(lambda x: x + '/COMPACT/', inner_dirs))
+            filelist = reduce(lambda x, y: x+y, map(get_files, self.inner_dirs))
+            self.filelist = sorted(filelist)
+        else:
+            self.filelist = filelist
         print('Added a total of {} compressed files.'.format(len(self.filelist)))
 
     @staticmethod
-    def file_to_trees(file: str) -> Sequence[Tuple[str, et.ElementTree]]:
+    def file_to_trees(file: str) -> Sequence[Tuple[str, Tree.ElementTree]]:
         blocks = split_xml(unzip(file))
         blocks, names = list(zip(*blocks))
         names = list(map(lambda name: file+name, names))
-        return list(zip(names, list(map(lambda block: et.ElementTree(et.fromstring(block)), blocks))))
+        return list(zip(names, list(map(lambda block: Tree.ElementTree(Tree.fromstring(block)), blocks))))
 
     @staticmethod
-    def file_to_projections(file: str, projector: Callable[[et.ElementTree], _T1]) -> Sequence[_T1]:
-        return list(filter(lambda x: x is not None,
-                           list(map(lambda tree: projector(tree), DatasetMaker.file_to_trees(file)))))
+    def file_to_projections(file: str, projector: Callable[[Tree.ElementTree], Projections]) -> Projections:
+        ret = list(chain.from_iterable([projector(tree) for _, tree in DatasetMaker.file_to_trees(file)]))
+        return [r for r in ret if r is not None]
 
-    def iterate_data(self, projector: Callable[[et.ElementTree], _T1], save_to: str = 'dump', start_from: int = 0):
-        with open(save_to, 'a') as f:
-            if start_from == 0:
-                f.write('<?xml version="1.0"?>\n<sentences>\n')
-            for i, file in tqdm(enumerate(self.filelist[start_from:])):
-                projections = list(chain.from_iterable(self.file_to_projections(file, projector)))
-                xmls = '\n'.join(list(map(lambda p: self.projection_to_xml(*p), projections)))
-                f.write(xmls)
-                with open('last_file.txt', 'w') as g:
-                    g.write(str(i+start_from))
+    def iterate_data(self, projector: Callable[[Tree.ElementTree], Projections], save_to: str = './TypeLM/data/dump',
+                     start_from: int = 0) -> None:
+        for i, file in tqdm(enumerate(self.filelist)):
+            if i <= start_from:
+                continue
+            projections = self.file_to_projections(file, projector)
+            strs = '\n'.join(list(map(lambda p: self.project_to_str(*p), projections)))
+            with open(f'{save_to}/{i}.dmp', 'w') as dump:
+                dump.write(strs)
+            with open('./TypeLM/data/last_file.txt', 'w') as g:
+                g.write(str(i))
 
     @staticmethod
-    def projection_to_xml(name: str, words: Sequence[str], types: Sequence[str]) -> str:
-        word_str = '\t<words>\n\t\t' + \
-                   '\n\t\t'.join(list(map(lambda word: '<word>"' + word + '"</word>', words))) + \
-                   '\n\t</words>'
-        type_str = '\t<types>\n\t\t' \
-                   + '\n\t\t'.join(list(map(lambda type: '<type>"' + type + '"</type>', types))) + \
-                   '\n\t</types>'
-
-        return '<sentence id="' + name + '">' + '\n' + word_str + '\n' + type_str + '\n</sentence>'
+    def project_to_str(words: Sequence[str], types: Sequence[str]) -> str:
+        word_str = '\t'.join(words)
+        type_str = '\t'.join(types)
+        return f'{word_str}\n{type_str}\n'
 
 
-# dsmk = DatasetMaker(directory)
-# dsmk.iterate_data(compose)
+def compose(tree: Tree.ElementTree) -> Projections:
+    try:
+        dags = transformer(tree)
+        if dags is None:
+            return []
+        dags = [extractor(dag) for dag in dags]
+        tuples = [(get_words(dag),
+                   [t.polish() for t in get_types(dag)]) for dag in dags if dag is not None]
+        return tuples
+    except KeyError:
+        return []
+
+
+with open('./TypeLM/data/filelist.txt', 'r') as f:
+    filelist = f.read().split('\n')
+dsmk = DatasetMaker(None, filelist)
+dsmk.iterate_data(compose)
