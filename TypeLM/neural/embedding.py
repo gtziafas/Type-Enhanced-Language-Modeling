1f1500afc372c65f1fa7d347eb129b5fdaf92b43
konstantinosKokos <konstantinos@riseup.net>
2020-06-08 12:28:44 +0200

    add invertible embedder

---
+++ TypeLM/neural/embedding.py
@@ -0,0 +1,19 @@
+from torch.nn import Module
+import torch
+from torch import LongTensor, Tensor
+from torch.nn.functional import linear, embedding
+from math import sqrt
+
+
+class InvertibleEmbedder(Module):
+    def __init__(self, embedding_dim: int, num_embeddings: int, padding_idx: int, scale_by_sqrt: bool):
+        super(InvertibleEmbedder, self).__init__()
+        self.embedding_matrix = torch.nn.Parameter(data=torch.rand(embedding_dim, num_embeddings), requires_grad=True)
+        self.padding_idx = padding_idx
+        self.embedding_scale = sqrt(embedding_dim) if scale_by_sqrt else 1.
+
+    def embed(self, idxes: LongTensor) -> Tensor:
+        return embedding(idxes, self.embedding_matrix, self.padding_idx) * self.embedding_scale
+
+    def invert(self, weights: LongTensor):
+        return linear(weights, self.embedding_matrix)
