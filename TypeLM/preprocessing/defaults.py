c82cbdf0646110c44f187c4b0dbaf5b09d8e8988
konstantinosKokos <konstantinos@riseup.net>
2020-06-10 13:11:56 +0200

    add loader to defaults.py

--- TypeLM/preprocessing/defaults.py
+++ TypeLM/preprocessing/defaults.py
@@ -1,6 +1,9 @@
-from typing import Tuple, Sequence, Set, Callable
+from typing import Tuple, Sequence, Callable
 from TypeLM.preprocessing.tokenizer import Tokenizer
 from TypeLM.preprocessing.masker import Masker
+from TypeLM.preprocessing.loader import LazyLoader, Samples
+from TypeLM.neural.utils import pad_sequence
+from torch import Tensor, LongTensor, ones
 
 with open('./TypeLM/data/indexing/atomset.txt', 'r') as f:
     atomset = set(f.read().split('\n'))
@@ -31,3 +34,32 @@ def regularization_type_masker() -> Masker:
     replacements = set(_tokenizer.type_tokenizer.vocabulary.values()).difference(unmaskable)
     return Masker(outer_chance=0.01, mask_chance=0, keep_chance=0, replacements=replacements, token_mask=None,
                   unmaskable=unmaskable)
+
+
+def default_dataloader(path: str, chunk_size: int, batch_size: int) -> LazyLoader:
+    word_masker = default_word_masker()
+    type_masker = regularization_type_masker()
+
+    def post_proc(sentences: Samples):
+
+        true_words, true_types = list(zip(*sentences))
+
+        word_lens = list(map(len, true_words))
+
+        masked_words, masked_ids = list(zip(*list(map(word_masker, true_words))))
+        masked_types, _ = list(zip(*list(map(type_masker, true_types))))
+        masked_words = pad_sequence(list(map(LongTensor, masked_words)),
+                                    _tokenizer.word_tokenizer.core.pad_token_id)
+        true_words = pad_sequence(list(map(LongTensor, true_words)),
+                                  _tokenizer.word_tokenizer.core.pad_token_id)
+        true_types = pad_sequence(list(map(LongTensor, masked_types)),
+                                  _tokenizer.type_tokenizer.PAD_TOKEN_ID)
+        masked_ids = pad_sequence(list(map(LongTensor, masked_ids)),
+                                  0)
+        padding_mask = ones(true_words.shape[0], true_words.shape[1], true_words.shape[1])
+        # todo: write in pytorch ops
+        for i, l in enumerate(word_lens):
+            padding_mask[i, :, l::] = 0
+        return masked_words, true_words, true_types, padding_mask, masked_ids
+    return LazyLoader(path, chunk_size, batch_size, post_proc)
+
