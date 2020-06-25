b43045378fcbc593562bf9fa76c98f2abaaf85e1
konstantinosKokos <konstantinos@riseup.net>
2020-06-25 10:11:48 +0200

    small changes for the classifying dataset

--- TypeLM/preprocessing/tokenizer.py
+++ TypeLM/preprocessing/tokenizer.py
@@ -26,9 +26,10 @@ class WordTokenizer:
         return self.core.convert_ids_to_tokens(ids)
 
     def convert_sent_to_ids_and_wordstarts(self, sent: str) -> Tuple[ints, ints]:
-        tokens: strs = self.core.tokenize(sent)
-        word_starts: ints = [1 if not t.startswith('##') else 0 for i, t in enumerate(tokens)]
-        tokens = [self.core.cls_token] + tokens + [self.core.sep_token]
+        words = self.core.prepare_for_tokenization(sent).split()
+        tokens = [self.core.tokenize(word) for word in words]
+        word_starts = sum([[1] + [0] * (len(t) - 1) for t in tokens], [])
+        tokens = [self.core.cls_token] + sum(tokens, []) + [self.core.sep_token]
         return self.core.convert_tokens_to_ids(tokens), [0] + word_starts + [0]
 
 
@@ -70,14 +71,16 @@ class AtomTokenizer(TypeTokenizer):
 
 class FullTokenizer(TypeTokenizer):
     def __init__(self, vocabulary: Set[str]):
-        self.special_tokens = ['[PAD]', '[UNK]']
+        self.special_tokens = ['[PAD]', '[UNK]', '[MWU]']
         vocabulary = {k: i for i, k in enumerate(self.special_tokens + sorted(vocabulary))}
         self.vocabulary = defaultdict(lambda: vocabulary['[UNK]'], vocabulary)
         self.inverse_vocabulary = {v: k for k, v in self.vocabulary.items()}
         self.PAD_TOKEN = '[PAD]'
         self.UNK_TOKEN = '[UNK]'
+        self.MWU_TOKEN = '[MWU]'
         self.PAD_TOKEN_ID = self.vocabulary['[PAD]']
         self.UNK_TOKEN_ID = self.vocabulary['[UNK]']
+        self.MWU_TOKEN_ID = self.vocabulary['[MWU]']
 
     def __len__(self) -> int:
         return len(self.vocabulary)
@@ -102,16 +105,18 @@ class Tokenizer:
 
     def convert_pair_to_ids(self, sent: str, types: strs) -> Optional[Tuple[ints, ints]]:
         if isinstance(self.type_tokenizer, FullTokenizer):
+            words = sent.split('\t')
+            words = [w.split() for w in words]
+            types = [[t] + [self.type_tokenizer.MWU_TOKEN] * (len(w)-1) for w, t in zip(words, types)]
+            types = sum(types, [])
             s_ids, word_starts = self.word_tokenizer.convert_sent_to_ids_and_wordstarts(sent)
-            type_iter = iter(self.convert_types_to_ids(types))
-            t_ids = [self.type_tokenizer.PAD_TOKEN_ID if word_starts[i] == 0 else type_iter.__next__()
-                     for i in range(len(s_ids))]
-        elif isinstance(self.type_tokenizer, AtomTokenizer):
-            s_ids = self.convert_sent_to_ids(sent)
-            t_ids = self.convert_types_to_ids([self.type_tokenizer.SOS_TOKEN] + types)
+            t_ids = self.convert_types_to_ids(types)
+            type_iter = iter(t_ids)
+            t_ids = [self.type_tokenizer.PAD_TOKEN_ID if ws == 0 else type_iter.__next__()
+                     for ws in word_starts]
+            return s_ids, t_ids
         else:
             raise NotImplementedError
-        return s_ids, t_ids
 
 
 def _parse_line(line_: str) -> strs:
@@ -200,28 +205,35 @@ def _parse_dump_atomic(dump: str = './TypeLM/data/extraction/dump', start_from: 
                     h.write(str(read + 1))
 
 
-def _parse_dump_full(dump: str = './TypeLM/data/extraction/dump', start_from: int = 0) -> None:
+def _parse_dump_full(dump: str = './TypeLM/data/extraction/dump', start_from: int = 3774747) -> None:
     with open('./TypeLM/data/indexing/small_typeset.txt', 'r') as f:
         typeset = set(f.read().split('\n'))
     tokenizer = Tokenizer(type_vocabulary=typeset, atomic=False)
     with open(dump, 'r') as f:
         read = 0
-        with open('./TypeLM/data/indexing/atomic_dump', 'a') as g:
+        with open('./TypeLM/data/indexing/full_dump', 'a') as g:
             while True:
                 next_line = f.__next__().strip('\n')
                 if next_line == '':
-                    _ = f.__next__().strip('\n')
+                    sent = f.__next__().strip('\n')
+                else:
+                    sent = next_line
                 types = _parse_line(f.__next__())
 
                 read += 1
                 if read <= start_from:
                     continue
-
-                s_ids_int, t_ids_int = tokenizer.convert_pair_to_ids(sent, types)
-                s_ids = list(map(str, s_ids_int))
-                t_ids = list(map(str, t_ids_int))
-                line = f'{s_ids}\t{t_ids}\n'
-                g.write(line)
-
                 with open('./TypeLM/data/indexing/last_file', 'w') as h:
                     h.write(str(read + 1))
+                s_ids_int, t_ids_int = tokenizer.convert_pair_to_ids(sent, types)
+                if len(s_ids_int) != len(t_ids_int):
+                    print(f'failed at {read}')
+                    print(sent)
+                    print(types)
+                    continue
+                s_ids = list(map(str, s_ids_int))
+                t_ids = list(map(str, t_ids_int))
+                line = f'{" ".join(s_ids)}\t{" ".join(t_ids)}\n'
+                g.write(line)
+
+
