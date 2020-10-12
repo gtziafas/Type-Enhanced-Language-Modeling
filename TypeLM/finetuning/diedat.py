0ed2ac3cb19dc0765e3ca9d5b4435a5910d5fa3f
gtziafas <pullmypill@hotmail.com>
2020-10-12 15:04:20 +0300

    printing

--- TypeLM/finetuning/diedat.py
+++ TypeLM/finetuning/diedat.py
@@ -104,16 +104,17 @@ def main(diedat_path: str, model_path: str, device: str, batch_size_train: int, 
 
     sprint('Done with tokenization/loading, starting to train...')
     for epoch in range(num_epochs):
+        sprint(f'EPOCH {epoch+1}:')
         train_loss, train_accu = train_epoch(model, loss_fn, optim, train_loader, \
                 token_pad_id, word_pad_id, mask_token_id, device)
         sprint(f'Train loss:\t\t{train_loss:.5f}')
         sprint(f'Train accu:\t\t{train_accu:.5f}')
-        sprint('-' * 64)
+        sprint()
         val_loss, val_accu = eval_epoch(model, loss_fn, dev_loader, token_pad_id, \
                 word_pad_id, mask_token_id, device)
         sprint(f'Dev loss:\t\t{val_loss:.5f}')
         sprint(f'Dev accu:\t\t{val_accu:.5f}')
-        sprint('-' * 64)
+        sprint()
         test_loss, test_accu = eval_epoch(model, loss_fn, test_loader, token_pad_id, \
                 word_pad_id, mask_token_id, device)
         sprint(f'Test loss:\t\t{test_loss:.5f}')
